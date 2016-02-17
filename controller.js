22a5ea160602a1b270a3f2100a345bf4fa81d7e3
Kyle Prestgard <ktprestgard@gmail.com>
2016-02-16 18:10:53 -0700

    finished

---
+++ controller.js
@@ -0,0 +1,9 @@
+var app = angular.module('userProfiles');
+
+  app.controller('mainController', function($scope, mainService) {
+  	var getUsers = function() {
+  		$scope.users = mainService.getUsers();
+  	}
+  	
+  	getUsers();
+  })
